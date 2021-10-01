4b3947f5a058540fc7f8f6b904929399f2700d12
Corcoleison <pablocorcoles999@gmail.com>
2021-10-01 16:03:09 +0200

    Body and first lines

---
+++ game.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById("canvas");
+var crx = canvas.getContext('2d');
+canvas.style.width = canvas.width + "px";
+canvas.style.height = canvas.height + "px";
+input.offset = new Vector2(GetLeft(canvas), GetTop(canvas));
+
