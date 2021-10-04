71227bc2165d2247719a3618d62e2b9dd9ce2871
Corcoleison <pablocorcoles999@gmail.com>
2021-10-04 14:50:58 +0200

    first drawings

--- game.js
+++ game.js
@@ -1,6 +1,14 @@
 var canvas = document.getElementById("canvas");
-var crx = canvas.getContext('2d');
-canvas.style.width = canvas.width + "px";
-canvas.style.height = canvas.height + "px";
-input.offset = new Vector2(GetLeft(canvas), GetTop(canvas));
+var ctx = canvas.getContext('2d');
 
+ctx.beginPath();
+ctx.rect(200, 20, 50, 50);
+ctx.fillStyle="gray";
+ctx.fill();
+
+ctx.beginPath();
+ctx.moveTo(250, 80);
+ctx.lineTo(225, 100);
+ctx.lineTo(200, 80);
+ctx.fillStyle="orange";
+ctx.fill();
